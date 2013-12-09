ff67a76d1b93495fe37c078a84f285018f21732b
hwlts <hwlts@qq.com>
2013-12-09 19:59:20 +0800

    Max Points On A Line

--- Java/src/ACM/LeetCode/MaxPointsOnALine.java
+++ Java/src/ACM/LeetCode/MaxPointsOnALine.java
@@ -1,5 +1,7 @@
 package ACM.LeetCode;
 
+import java.util.*;
+
 /**
  * Given n points on a 2D plane,
  * find the maximum number of points that lie on the same straight line.
@@ -7,7 +9,7 @@ package ACM.LeetCode;
 
 public class MaxPointsOnALine
 {
-    static public class Point
+    public static class Point
     {
         int x;
         int y;
@@ -27,9 +29,81 @@ public class MaxPointsOnALine
 
     public int maxPoints(Point[] points)
     {
+        int len = points.length;
+        if (len < 2)
+            return len == 0 ? 0 : 1;
+        int max = 0;
+        for (int i = 0; i < len; ++i)
+        {
+            int sameX = 1;
+            int samePoint = 0;
+            HashMap<Double, Integer> h = new HashMap<Double, Integer>();
+            int j = 0;
+            while (j < i)
+            {
+                if (points[i].x == points[j].x && points[i].y == points[j].y)
+                {
+                    samePoint++;
+                    j++;
+                    continue;
+                }
+                if (points[i].x == points[j].x)
+                    ++sameX;
+                else
+                {
+                    double r = rate(points[i],points[j]);
+                    if (!h.containsKey(r))
+                        h.put(r,2);
+                    else
+                    {
+                        int t = h.get(r);
+                        h.put(r,t+1);
+                    }
+                }
+                j++;
+            }
+            j = i + 1;
+            while (j < len)
+            {
+                if (points[i].x == points[j].x && points[i].y == points[j].y)
+                {
+                    samePoint++;
+                    j++;
+                    continue;
+                }
+                if (points[i].x == points[j].x)
+                    ++sameX;
+                else
+                {
+                    double r = rate(points[i],points[j]);
+                    if (!h.containsKey(r))
+                        h.put(r,2);
+                    else
+                    {
+                        int t = h.get(r);
+                        h.put(r,t+1);
+                    }
+                }
+                j++;
+            }
 
-        return 0;
+            int h_max = 1;
+            Iterator iter = h.entrySet().iterator();
+            while (iter.hasNext())
+            {
+                Map.Entry entry = (Map.Entry) iter.next();
+                int val = (Integer) entry.getValue();
+                h_max = val > h_max ? val : h_max;
+            }
+
+            int t_max = sameX > h_max ? sameX : h_max;
+            max = (t_max + samePoint) > max ? (t_max + samePoint) : max;
+        }
+        return max;
     }
 
-
+    public static double rate(Point a, Point b)
+    {
+        return ((double)a.y-(double)b.y)/((double)a.x-(double)b.x);
+    }
 }
