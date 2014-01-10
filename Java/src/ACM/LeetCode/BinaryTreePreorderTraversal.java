34784ee32bafbd48c72c52349fe58e18a534acfe
hwlts <hwlts@qq.com>
2014-01-10 21:53:27 +0800

    EvaluateReversePolishNotation

---
+++ Java/src/ACM/LeetCode/BinaryTreePreorderTraversal.java
@@ -0,0 +1,73 @@
+package ACM.LeetCode;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * For example:
+ * Given binary tree {1,#,2,3},
+ * 1
+ *  \
+ *   2
+ *  /
+ * 3
+ * return [1,2,3].
+ *
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ */
+
+public class BinaryTreePreorderTraversal
+{
+    static class TreeNode
+    {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public ArrayList<Integer> preorderTraversal(TreeNode root)
+    {
+        if (root == null)
+            return new ArrayList<Integer>();
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode p = root;
+
+        while (p != null || !stack.empty())
+        {
+            if (p != null)
+            {
+                result.add(p.val);
+                if (p.right != null)
+                    stack.push(p.right);
+                p = p.left;
+            }
+            else
+            {
+                p = stack.peek();
+                stack.pop();
+            }
+        }
+
+        return result;
+    }
+
+    static void outPutList(ArrayList l)
+    {
+        for (Object o : l)
+        {
+            System.out.println(o);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        TreeNode t = new TreeNode(1);
+
+        ArrayList<Integer> a = new BinaryTreePreorderTraversal().preorderTraversal(t);
+        outPutList(a);
+    }
+}
